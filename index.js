bcfe1a284f79b565cf525c31ee9cae615412aa53
Harrison <harrison.standeffer@slalom.com>
2025-09-10 12:38:10 -0500

    fix typo

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ app.get('/health', (req, res) => res.json({ status: 'ok' }));
 if (require.main === module) {
   app.listen(3000, '0.0.0.0', () => console.log('API running on port 3000'));
 }
-module.exports = app;l
+module.exports = app;
